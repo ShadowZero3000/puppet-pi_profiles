2e90e43a7f30ca071f7f7e4ebe595e6e3f543590
Josh Souza <git@pureinsomnia.com>
2018-02-14 13:00:12 -0700

    Disable letsencrypt for now

--- manifests/ssl.pp
+++ manifests/ssl.pp
@@ -2,9 +2,11 @@
 class pi_profiles::ssl (
   $certs = {}
 ){
-  include ::letsencrypt
+  # This module depends on the package being available. I've just installed from scratch and am now using
+  # certbot. When I have more time, it'd be good to fix this.
+  # include ::letsencrypt
 
-  if ($certs != {}) {
-    create_resources(letsencrypt::certonly, $certs)
-  }
+  # if ($certs != {}) {
+  #   create_resources(letsencrypt::certonly, $certs)
+  # }
 }
